96615d68bb14085f63ae4c58782a01916ead5fae
Kritesh Timsina <kriteshtimsina55@gmail.com>
2023-06-14 10:38:13 +0545

    refactor code

--- src/main.ts
+++ src/main.ts
@@ -2,90 +2,57 @@ const button = document.querySelector("#generate-pwd");
 const display: HTMLInputElement | null = document.querySelector("#display");
 const passwordLength: HTMLInputElement | null =
   document.querySelector("#length");
-const uppercase: HTMLInputElement | null = document.querySelector("#uppercase");
-const lowercase: HTMLInputElement | null = document.querySelector("#lowercase");
-const numbers: HTMLInputElement | null = document.querySelector("#numbers");
-const symbols: HTMLInputElement | null = document.querySelector("#specials");
-const clipboard = document.querySelector("#clipboard");
+const options: any = document.querySelectorAll(".select-box input");
+const clipboard: HTMLButtonElement | null =
+  document.querySelector("#clipboard");
 
-const lowercaseChars: string = "abcdefghijklmnopqrstuvwxyz";
-const uppercaseChars: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numberChars: string = "0123456789";
-const specialsChars: string = "!@#$%^&*_-";
+type IChar = {
+  lowercase: string;
+  uppercase: string;
+  numbers: string;
+  symbols: string;
+};
 
-function getCheckedValue() {
-  let value = "";
+const characters: IChar | any = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numbers: "0123456789",
+  symbols: "!@#$%^&*_-",
+};
 
-  if (lowercase?.checked) {
-    value = lowercaseChars;
-  }
-  if (uppercase?.checked) {
-    value = uppercaseChars;
-  }
-  if (numbers?.checked) {
-    value = numberChars;
-  }
-  if (symbols?.checked) {
-    value = specialsChars;
-  }
-  if (symbols?.checked && lowercase?.checked) {
-    value = specialsChars + lowercaseChars;
-  }
-  if (symbols?.checked && uppercase?.checked) {
-    value = specialsChars + uppercaseChars;
-  }
-  if (symbols?.checked && numbers?.checked) {
-    value = specialsChars + numberChars;
-  }
-  if (numbers?.checked && lowercase?.checked) {
-    value = numberChars + lowercaseChars;
-  }
-  if (numbers?.checked && uppercase?.checked) {
-    value = numberChars + uppercaseChars;
-  }
+function getCheckedValue() {}
 
-  if (lowercase?.checked && uppercase?.checked) {
-    value = lowercaseChars + uppercaseChars;
-  }
-  if (lowercase?.checked && uppercase?.checked && numbers?.checked) {
-    value = lowercaseChars + uppercaseChars + numberChars;
-  }
-  if (
-    lowercase?.checked &&
-    uppercase?.checked &&
-    numbers?.checked &&
-    symbols?.checked
-  ) {
-    value = lowercaseChars + uppercaseChars + numberChars + specialsChars;
-  }
-  return value;
-}
-
-function handleSubmit(e: any) {
-  e.preventDefault();
-  const password = generatePassword();
-  display!.value = password;
-
-  clipboard?.addEventListener("click", async (e) => {
-    e.preventDefault();
-    await navigator.clipboard.writeText(password);
-    clipboard.innerHTML = "&check;";
-    setTimeout(() => {
-      clipboard.innerHTML = "&#128203;";
-    }, 3000);
+function generateRandomPassword() {
+  let passwordCharacters: string = "";
+  let generatedPassword: string = "";
+  options!.forEach((option: any) => {
+    if (option.checked) {
+      passwordCharacters += characters[option.id];
+    }
   });
-}
 
-function generatePassword() {
-  const passwordChars = getCheckedValue();
-
-  let pass: string = "";
   for (let i = 0; i < Number(passwordLength?.value); i++) {
-    const randomChars = Math.floor(Math.random() * passwordChars?.length);
-    pass += passwordChars?.substring(randomChars, randomChars + 1);
+    const randomChars = Math.floor(Math.random() * passwordCharacters?.length);
+    generatedPassword += passwordCharacters?.substring(
+      randomChars,
+      randomChars + 1
+    );
   }
+  display!.value = generatedPassword;
+}
+generateRandomPassword();
 
-  return pass;
+function copyToClipboard(e: any) {
+  e.preventDefault();
+  navigator.clipboard.writeText(display!.value);
+  clipboard!.innerHTML = `<i class="fa-regular fa-circle-check"></i>`;
+  setTimeout(() => {
+    clipboard!.innerHTML = `<i class="fa-regular fa-clipboard"></i>`;
+  }, 3000);
 }
 
-button?.addEventListener("click", handleSubmit);
+clipboard?.addEventListener("click", copyToClipboard);
+button?.addEventListener("click", (e) => {
+  e.preventDefault();
+  generateRandomPassword();
+});
